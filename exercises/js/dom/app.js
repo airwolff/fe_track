681830a737f10cc2cd8825ebb071853955072a17
a1rwolff <andy.wolff@martinwilliams.com>
2020-01-07 14:39:32 -0600

    get element practice

--- exercises/js/dom/app.js
+++ exercises/js/dom/app.js
@@ -1,6 +1,7 @@
 const myHeading = document.getElementById('myHeading');
 const myButton = document.getElementById('myButton');
+const myTextInput = document.getElementByi('myTextInput');
 
 myButton.addEventListener('click', () => {
-  myHeading.style.color = 'red';
+  myHeading.style.color = myTextInput.value;
 });
