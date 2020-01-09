2ddd0ea6f93dc41f83a4b779f167d39667c288bf
a1rwolff <andy.wolff@martinwilliams.com>
2020-01-09 13:34:07 -0600

    get tags

--- exercises/js/dom/app.js
+++ exercises/js/dom/app.js
@@ -1,7 +1,13 @@
-const myHeading = document.getElementById('myHeading');
-const myButton = document.getElementById('myButton');
-const myTextInput = document.getElementByi('myTextInput');
+// const myHeading = document.getElementById('myHeading');
+// const myButton = document.getElementById('myButton');
+const myTextInput = document.getElementById('myTextInput');
+// const myHeading = document.getElementsByTagName('h1')[0];
 
-myButton.addEventListener('click', () => {
-  myHeading.style.color = myTextInput.value;
+// myButton.addEventListener('click', () => {
+//   myHeading.style.color = myTextInput.value;
+// });
+const myList = document.getElementsByTagName('li')[0];
+
+myList.addEventListener('click', () => {
+	myList.style.color = myTextInput.value;
 });
--- notes/notes.js
+++ notes/notes.js
@@ -133,3 +133,31 @@ const myHeading = document.getElementById('myHeading');
 myHeading.addEventListener('click', () => {
   myHeading.style.color = 'red';
 });
+
+const myParagraph = document.getElementsByTagName('p');
+
+
+
+
+//////////////////////////////////////////////////////////
+//  Traversing DOM
+//////////////////////////////////////////////////////////
+// DOM methods return a collection not a JavaScript array, the collection is called an HTMLCollection, an “array-like” object.  Iterate over it with a for loop, but
+//methods such as map, filter, and reduce aren't available.
+// Can convert an HTMLCollection to an array by using the spread operator: [...document.getElementsByTagName(p)].
+
+const callouts = document.getElementsByClassName('callout');
+// OR
+const callouts = document.getElementById('callout');
+
+const paragraphs = document.getElementsByTagName('p'); //
+
+const para1 = document.getElementsByTagName('p')[0]; // first p tag
+
+element = document.querySelector(selectors); //A DOMString containing one or more selectors to match. This string must be a valid CSS selector string;
+
+para1.textContent; // "This is a simple HTML file."
+para1.innerHTML; // "This is a <i>simple</i> HTML file."
+para1.textContent = 'Modified HTML file'; // look for change in browser
+para1.innerHTML = '<i>Modified</i> HTML file'; // look for change in browser
+
